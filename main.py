dc55ec862592e2886cacd8fbbda6fdf8fd7ee110
loicmagne <loic.magne@outlook.com>
2021-03-23 16:49:54 +0100

    Refactored ICP registration code

--- main.py
+++ main.py
@@ -4,12 +4,20 @@ from tqdm import tqdm
 
 from frameseq import *
 
-def point2pointICP(frameSeq,threshold=0.5,iterations=100,inits=None):
+def ICP(frameSeq, registration_technique='point2point', inits=None, threshold=0.5, iterations=50):
     transf = [np.eye(4)]
     current = frameSeq[0]
     for k in tqdm(range(1,len(frameSeq))):
         nxt = frameSeq[k]
         init = np.eye(4) if (inits is None) else inits[k]
+        reg_p2p = registrationICP(current, nxt, threshold, iterations, init,
+                                  registration_technique)
+        current = nxt
+        transf.append(reg_p2p.transformation)
+    return transf
+
+def registrationICP(current, nxt, threshold, iterations, init, registration_technique):
+    if registration_technique == 'point2point':
         cloud1 = current.cloud
         cloud2 = nxt.cloud
         reg_p2p = o3d.pipelines.registration.registration_icp(
@@ -17,17 +25,7 @@ def point2pointICP(frameSeq,threshold=0.5,iterations=100,inits=None):
             estimation_method=o3d.pipelines.registration.TransformationEstimationPointToPoint(),
             criteria=o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=iterations)
         )
-        current = nxt
-        transf.append(reg_p2p.transformation)
-    return transf
-
-
-def point2planeICP(frameSeq,threshold=0.5,iterations=1000,inits=None):
-    transf = [np.eye(4)]
-    current = frameSeq[0]
-    for k in tqdm(range(1,len(frameSeq))):
-        nxt = frameSeq[k]
-        init = np.eye(4) if (inits is None) else inits[k]
+    elif registration_technique == 'point2plane':
         cloud1 = current.cloud
         cloud2 = nxt.cloud
         cloud1.estimate_normals(
@@ -39,17 +37,7 @@ def point2planeICP(frameSeq,threshold=0.5,iterations=1000,inits=None):
             estimation_method=o3d.pipelines.registration.TransformationEstimationPointToPlane(),
             criteria=o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=iterations)
         )
-        current = nxt
-        transf.append(reg_p2p.transformation)
-    return transf
-
-
-def plane2planeICP(frameSeq,threshold=0.5,iterations=100,inits=None):
-    transf = [np.eye(4)]
-    current = frameSeq[0]
-    for k in tqdm(range(1,len(frameSeq))):
-        nxt = frameSeq[k]
-        init = np.eye(4) if (inits is None) else inits[k]
+    elif registration_technique == 'plane2plane':
         cloud1 = current.cloudParam
         cloud2 = nxt.cloudParam
         reg_p2p = o3d.pipelines.registration.registration_icp(
@@ -57,13 +45,11 @@ def plane2planeICP(frameSeq,threshold=0.5,iterations=100,inits=None):
             estimation_method=o3d.pipelines.registration.TransformationEstimationPointToPoint(),
             criteria=o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=iterations)
         )
-        current = nxt
-        transf.append(reg_p2p.transformation)
-    return transf
+    return reg_p2p
 
 if __name__ == '__main__':
-    seq = FrameSeq([str(10*k) for k in range(21)][6:8],precomputed=True)
+    seq = FrameSeq([str(10*k) for k in range(21)][8:10],precomputed=True)
     seq.display()
-    seq.transform(point2planeICP(seq))
-    print(np.asarray(seq[0].cloudParam.points))
+    init = ICP(seq,registration_technique='point2point', inits=None)
+    seq.transform(ICP(seq,registration_technique='point2plane', inits=init))
     seq.display()
